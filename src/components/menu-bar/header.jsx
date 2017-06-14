a8acffab6b9b60677167cfc92e6b46af13ef6cd0
zhouguang <zhouguang@kenrobot.com>
2017-06-14 20:38:33 +0800

    remove f=master

--- src/components/menu-bar/header.jsx
+++ src/components/menu-bar/header.jsx
@@ -31,9 +31,9 @@ class Header extends React.Component {
                 <a className={styles.logo} href="http://www.kenrobot.com" target="_blank"><img src={logo} /></a>
                 <ul className={styles.nav}>
                     <li><a href="http://www.kenrobot.com" target="_blank">首页</a></li>
-                    <li><a href="http://edu.kenrobot.com#f=master" target="_blank">教育版</a></li>
-                    <li><a href="http://ide.kenrobot.com#f=master" target="_blank">开发版</a></li>
-                    <li><a href="http://scratch.kenrobot.com#f=master" target="_blank">Scratch3(Beta版)</a></li>
+                    <li><a href="http://edu.kenrobot.com" target="_blank">教育版</a></li>
+                    <li><a href="http://ide.kenrobot.com" target="_blank">开发版</a></li>
+                    <li><a href="http://scratch.kenrobot.com" target="_blank">Scratch3(Beta版)</a></li>
                     <li><a href="http://www.kenrobot.com/index.php?app=square&mod=Index&act=help" target="_blank">帮助</a></li>
                 </ul>
             </div>
