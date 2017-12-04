5ef2181a65a41f0db1de97c0d52a7fc6b499e723
zhouguang <zhouguang@kenrobot.com>
2017-12-04 19:00:29 +0800

    merge from git

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 webpackJsonp([1],{
 
-/***/ 134:
+/***/ 106:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -178,4 +178,4 @@ webpackJsonp([1],{
 
 /***/ })
 
-},[134]);
+},[106]);
