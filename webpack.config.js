a0582dd875ac1ecffbd0c4308ba63bb2fd9cf262
zhouguang <zhouguang@kenrobot.com>
2017-12-05 14:06:17 +0800

    fix bug

--- webpack.config.js
+++ webpack.config.js
@@ -75,8 +75,7 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            // 'process.env.NODE_ENV': '"' + process.env.NODE_ENV + '"',
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+            'process.env.NODE_ENV': '"' + process.env.NODE_ENV + '"',
             'process.env.DEBUG': Boolean(process.env.DEBUG)
         }),
         new webpack.optimize.CommonsChunkPlugin({
